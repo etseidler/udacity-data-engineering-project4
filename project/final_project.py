9f942921eb4b7c45daca903155ac6d8df9a2f827
Eric Seidler <eric.seidler@gmail.com>
2023-10-11 11:01:09 -0500

    chg: add other starter files

--- project/final_project.py
+++ project/final_project.py
@@ -3,11 +3,11 @@ import pendulum
 import os
 from airflow.decorators import dag
 from airflow.operators.dummy_operator import DummyOperator
-from final_project_operators.stage_redshift import StageToRedshiftOperator
-from final_project_operators.load_fact import LoadFactOperator
-from final_project_operators.load_dimension import LoadDimensionOperator
-from final_project_operators.data_quality import DataQualityOperator
-from udacity.common import final_project_sql_statements
+from .stage_redshift import StageToRedshiftOperator
+from .load_fact import LoadFactOperator
+from .load_dimension import LoadDimensionOperator
+from .data_quality import DataQualityOperator
+from . import final_project_sql_statements
 
 
 default_args = {
