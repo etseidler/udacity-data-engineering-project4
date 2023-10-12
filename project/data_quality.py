c6ac7c65ffe4c242316cc674c920e367ebb0d48f
Eric Seidler <eric.seidler@gmail.com>
2023-10-12 14:16:37 -0500

    chg: parameterize quality check sql

--- project/data_quality.py
+++ project/data_quality.py
@@ -5,18 +5,19 @@ from airflow.utils.decorators import apply_defaults
 class DataQualityOperator(BaseOperator):
 
     ui_color = '#89DA59'
-    has_some_rows_sql = """SELECT COUNT(*) FROM {}"""
 
     @apply_defaults
     def __init__(self,
                  redshift_conn_id="redshift",
                  tables=[],
+                 quality_check_sql = 'SELECT COUNT(*) FROM {}',
                  expected_min_rows=1,
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.tables = tables
+        self.quality_check_sql = quality_check_sql
         self.expected_min_rows = expected_min_rows
 
     def execute(self, context):
@@ -25,7 +26,7 @@ class DataQualityOperator(BaseOperator):
 
         for table in self.tables:
             redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
-            formatted_sql = DataQualityOperator.has_some_rows_sql.format(table)
+            formatted_sql = self.quality_check_sql.format(table)
             records = redshift.get_records(formatted_sql)
             if len(records) < self.expected_min_rows or len(records[0]) < self.expected_min_rows:
                 raise ValueError(f"Data quality check failed. {table} returned no results")
