e0d1b129ee50bdd8556beac4d6c891cabfbf3c7d
Eric Seidler <eric.seidler@gmail.com>
2023-10-12 14:18:36 -0500

    chore: add line to startup script to add data connections

--- exercises/startup.sh
+++ exercises/startup.sh
@@ -1,6 +1,7 @@
 /opt/airflow/start-services.sh
 /opt/airflow/start.sh
 airflow users create --email student@example.com --firstname aStudent --lastname aStudent --password XXXXX --role Admin --username XXXXX
+/home/workspace/airflow/dags/cd0031-automate-data-pipelines/set_connections.sh
 airflow scheduler
 
 # teardown
